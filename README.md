ec8c25581f77f435e3320158838f656c477e5176
Bh Ma <blownhitherma@gmail.com>
2019-10-30 00:33:48 -0400

    [Feature] SOD mini dataset; load data

--- README.md
+++ README.md
@@ -4,3 +4,20 @@ Salient Object Replacement in 2D Images
 with direct supervision from image synthesis
 
 
+### Datasets
+- Super small dataset of SOD (Salient Objects Dataset).   
+    Descriptions: http://elderlab.yorku.ca/SOD/#download      
+    - http://elderlab.yorku.ca/SOD/SOD.zip
+    - https://www2.eecs.berkeley.edu/Research/Projects/CS/vision/grouping/segbench/BSDS300-images.tgz
+    - decompress into SOD/ and BSDS300/ 
+    ![](https://i.ibb.co/py0tSKk/masks.png)
+    
+    
+    
+    ```python
+  import numpy as np
+  from load_data import load_parsed_data
+  images, masks = load_parsed_data()
+  assert masks[0].dtype == np.bool
+
+    ```
---
+++ load_data.py
@@ -0,0 +1,78 @@
+import numpy as np
+from pathlib import Path
+from scipy.io import loadmat
+from skimage.io import imread
+from skimage.draw import line, polygon
+import pprint
+from matplotlib import pyplot as plt
+
+
+def parse_sod():
+    """
+    Super small dataset of SOD (Salient Objects Dataset). Load in memory
+    Descriptions: http://elderlab.yorku.ca/SOD/#download
+
+    - http://elderlab.yorku.ca/SOD/SOD.zip
+    - https://www2.eecs.berkeley.edu/Research/Projects/CS/vision/grouping/segbench/BSDS300-images
+    .tgz
+
+    Return images and boolean mask
+    """
+    index = loadmat('SOD/DBidx.mat', squeeze_me=True, struct_as_record=False)
+    ret_images = []
+    ret_masks = []
+
+    for image_id in index['SFprefix'].flatten():
+        image_path = list(Path('BSDS300/images/').rglob(str(image_id) + '.jpg'))[0]
+        raw_image = imread(image_path)
+
+        sessions = loadmat('SOD/SO' + str(image_id) + '.mat', squeeze_me=True, struct_as_record=False)['SES']
+        for sess in sessions:
+            # TODO: use other sessions
+            # sess_id = sess.session
+            if isinstance(sess.obj, np.ndarray):
+                # The most salient object has imp=1.
+                salient_obj = next((o for o in sess.obj if o.IMP == 1), None)
+                if salient_obj is None:
+                    continue
+            else:
+                salient_obj = sess.obj
+
+            boundary = salient_obj.BND
+            if boundary.dtype == np.object:
+                # TODO: allow disconnected area
+                boundary = boundary[0]
+            mask = np.zeros(sess.ImSize.tolist(), dtype=np.bool)
+            rr, cc = polygon(boundary[:, 0], boundary[:, 1], sess.ImSize.tolist())
+            mask[rr, cc] = 1
+
+            ret_images.append(raw_image)
+            ret_masks.append(mask)
+            break
+    return ret_images, ret_masks
+
+
+def load_parsed_sod():
+    d = np.load('data/sod-pairs.npz', allow_pickle=True)
+    images, masks = d['images'], d['masks']
+    return images, masks
+
+
+def visualize_sod():
+    images, masks = load_parsed_sod()
+    for i in range(4):
+        ind = np.random.randint(0, len(images))
+        plt.subplot(4, 2, i * 2 + 1)
+        plt.imshow(images[ind])
+        plt.subplot(4, 2, i * 2 + 2)
+        plt.imshow(masks[ind])
+    plt.show()
+
+
+if __name__ == '__main__':
+    # images, masks = parse_data()
+    # np.savez('data/sod-pairs.npz', images=images, masks=masks)
+    visualize_sod()
+
+
+
