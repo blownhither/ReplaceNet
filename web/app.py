d85100f13069854badcf39d43f1c587842b54adb
Bh Ma <blownhitherma@gmail.com>
2019-12-15 01:57:07 -0500

    [Feature] frontend/backend

--- web/app.py
+++ web/app.py
@@ -1,12 +1,20 @@
+import sys
+import cv2
+import time
 import base64
 import skimage
+import logging
+import numpy as np
 from skimage.io import imread
-from flask import Flask, request
+from flask import Flask, request, send_file, send_from_directory, jsonify
 from flask_cors import CORS
 
 
 app = Flask(__name__)
 CORS(app)
+# app.logger.set
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.addHandler(logging.FileHandler('app.log', mode='a'))
 
 
 def view_base64_image(base64_string):
@@ -17,22 +25,54 @@ def view_base64_image(base64_string):
 
     imgdata = base64.b64decode(base64_string)
     img = imread(imgdata, plugin='imageio')
-    print(img)
+    # print(img)
     plt.imshow(img)
     plt.show()
 
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET'])
+def get_index():
+    return send_file('index.html')
+
+
+@app.route('/index.js', methods=['GET'])
+def get_index_js():
+    return send_file('index.js')
+
+
+@app.route('/style.css', methods=['GET'])
+def get_style_css():
+    return send_file('style.css')
+
+
+@app.route('/assets/<path:filename>')
+def send_assets_file(filename):
+    return send_from_directory('assets', filename, as_attachment=False)
+
+
+@app.route('/replace', methods=['POST'])
 def test_post():
-    # print(request.files)
-    # print(request.form)
-    # print(type(request.form['background']))
-    # print(request.json)
     view_base64_image(request.form['background'][len('data:image/png;base64,'):])
     view_base64_image(request.form['mask'][len('data:image/png;base64,'):])
-    return 'OK'
+    foreground_id = request.form['foreground_id']
+    print(foreground_id)
+
+    mat = skimage.img_as_ubyte(imread('assets/random.jpg'))
+
+    start = time.time()
+    r, g, b = np.split(mat, 3, axis=2)
+    mat = np.dstack([b, g, r])  # cv2 uses BGR order
+    success, content = cv2.imencode('.png', mat)
+    content = content.tobytes()
+    log_str = f'imwrite takes {time.time() - start}'
+    app.logger.warning(log_str)
+
+    return jsonify({
+        'img': base64.b64encode(content).decode(),
+        'status': 'OK'
+    })
 
 
 if __name__ == '__main__':
-    app.run(port=2951, debug=True)
+    app.run(host='0.0.0.0', port=2951, debug=True)
 
--- web/index.js
+++ web/index.js
@@ -24,6 +24,7 @@ function crop_and_resize(image, size) {
     image.width(size);
 }
 
+
 $(document).ready(function () {
     // Adapted from Konva tutorial
     let container = $('#pad_container');
@@ -62,6 +63,9 @@ $(document).ready(function () {
         image: canvas,
         x: (stage.width() - canvas.width) / 2,
         y: (stage.height() - canvas.height) / 2,
+        // the following creates border (but the border will be collected)
+        // stroke: "#ffa210",
+        // shadowBlur: 4,
     });
     layer.add(image);
     stage.draw();
@@ -135,7 +139,20 @@ $(document).ready(function () {
         layer.batchDraw();
     }
 
-    (function _bind_button() {
+    // foreground selection
+    let selected_foreground = "1";
+    function foreground_click(event) {
+        // handler for click on foreground images
+        let old_elem = $(`img[data-fg-id='${selected_foreground}']`);
+        old_elem.removeClass('selected_image');
+
+        let elem = $(event.target);
+        let new_id = elem.attr('data-fg-id');
+        elem.addClass('selected_image');
+        selected_foreground = new_id;
+    }
+    
+    (function _bind_buttons() {
         let backgroud_image_copy = null;
         $('#upload_button').change(function (event) {
             backgroud_image_copy = Konva.Image.fromURL(URL.createObjectURL(event.target.files[0]), function (image) {
@@ -155,25 +172,34 @@ $(document).ready(function () {
                             // let form = new FormData();
                             // form.append('background', background);
                             // form.append('mask', mask);
+                            let output_img_elem = $('#output-img');
+                            output_img_elem.attr('src', 'assets/loading.gif');
                             $.ajax({
-                                url: "http://localhost:2951",
+                                url: "http://localhost:2951/replace",
                                 method: "POST",
                                 data: {
                                     background: background.src, // string, base64
                                     mask: mask.src,
+                                    foreground_id: selected_foreground,
                                 },
                                 dataType: 'json'
                                 // processData: false,
                                 // contentType: false,
+                            }).done(function (data) {
+                                console.log(data);
+                                output_img_elem.attr('src', 'data:image/png;base64,' + data['img']);
+                            }).fail(function (data) {
+                                console.log('Ajax failed');
+                                console.log(data);
+                                output_img_elem.attr('src', 'assets/failed.png');
                             });
                         }
                     });
                 }
             });
-
-
-            // let image = $('#upload_button')[0].files[0];
-
         });
+
+        $('#clear_button').click(_reset_mask);
+        $('img[data-fg-id]').click(foreground_click);
     })();
 });
